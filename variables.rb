9b84cc78476c9d5b367900f189bc6a9ca4ce3c01
Wil Fady <wrfady@gmail.com>
2023-03-25 19:40:27 -0400

    Fix birthdate

--- variables.rb
+++ variables.rb
@@ -1,15 +1,27 @@
 email = "wrfady@gmail.com"
+puts email 
 first_name = Wil
+puts first_name
 last_name = Fady
+puts last_name
 
 age = 32
-birthdate = 08011990
+puts age
+birthdate = 811990
 grad_year = 2008
 
 pi = 3.14
 account_balance = -1200.00
+puts account_balance
 weight = 180.5
 
 deposit_paid = true
 currently_online = false
 received_email = true
+
+email = "btsady@gmail.com"
+puts email
+first_name = Bill
+puts first_name
+last_name = Sady
+puts last_name
